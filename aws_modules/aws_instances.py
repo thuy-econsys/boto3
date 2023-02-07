9b11f67b425374a0f5abcee35bb8366fc7018d79
Thuy Ngo <thuy.ngo@econsys.com>
2023-02-07 17:39:34 -0500

    add Null check

--- aws_modules/aws_instances.py
+++ aws_modules/aws_instances.py
@@ -16,7 +16,7 @@ RDS_CLIENT = RDS_CLIENT.describe_db_instances()
 EC2_CLIENT = boto3.client('ec2', region_name=REGION)
 EC2_RESOURCE = boto3.resource('ec2', region_name=REGION)
 
-reservations = EC2_CLIENT.describe_instances(
+RESERVATIONS = EC2_CLIENT.describe_instances(
     Filters=[
         {
             "Name": "instance-state-name",
@@ -97,7 +97,7 @@ def print_list(print_this: list):
 
 def print_rds_instances():
     if not RDS_CLIENT:
-        print("No RDS instances to print.")
+        print("No RDS instances to output.")
         return
 
     for rds in RDS_CLIENT['DBInstances']:
@@ -119,8 +119,10 @@ def print_rds_instances():
 # aws ec2 get-ebs-encryption-by-default - -region us-gov-west-1
 # aws ec2 describe-route-tables --filter "Name=vpc-id,Values=<vpc_id>" --query "RouteTables[*].{RouteTableId:RouteTableId, VpcId:VpcId, Routes:Routes, AssociatedSubnets:Associations[*].SubnetId}"
 def print_ec2_instances():
-    for reservation in reservations:
+    if not RESERVATIONS:
+        print('No EC2 instances to output.')
 
+    for reservation in RESERVATIONS:
         for ec2 in reservation['Instances']:
             print(f'{ec2["InstanceId"]} created by {ec2["ImageId"]} in {ec2["VpcId"]}, state: {ec2["State"]["Name"]}')
             print(f"has {ec2['BlockDeviceMappings'][0]['Ebs']['VolumeId']} status: {ec2['BlockDeviceMappings'][0]['Ebs']['Status']}")
@@ -130,6 +132,10 @@ def print_ec2_instances():
 
 
 def sort_and_filter_amis(days_to_retain: int = 30) -> list:
+    if not AMI_RESOURCE:
+        print("No AMIs found matching filter.")
+        return []
+
     ami_names = []
     retention_date = TODAY - timedelta(days=days_to_retain)
 
@@ -149,6 +155,10 @@ def sort_and_filter_amis(days_to_retain: int = 30) -> list:
 
 
 def keep_latest_amis(ami_names: list, retain_no: int = 5) -> list:
+    if not ami_names:
+        print("No AMIs up for deregistering.")
+        return []
+
     builds_to_keep = []
     build_names = []
     count = 0
@@ -175,6 +185,10 @@ def keep_latest_amis(ami_names: list, retain_no: int = 5) -> list:
 
 
 def deregister_amis(keep_builds: list) -> tuple:
+    if not keep_builds:
+        print("No AMIs to deregister.")
+        return ([], [])
+
     ec2_dependencies = []
     amis_retained = []
     amis_deregistered = []
@@ -208,6 +222,10 @@ def deregister_amis(keep_builds: list) -> tuple:
 
 
 def delete_snapshots(amis_retained: list, days_to_retain_snapshot: int = 90):
+    if not amis_retained:
+        print("No AMIs associated to snapshots for deletion.")
+        return
+
     snapshot_retention_date = TODAY - timedelta(days=days_to_retain_snapshot)
 
     # for ami in AMI_CLIENT['Images']:
@@ -240,6 +258,10 @@ def delete_snapshots(amis_retained: list, days_to_retain_snapshot: int = 90):
 
 
 def delete_volumes(days_to_retain_volume: int = 1096):
+    if not VOLUME_RESOURCE:
+        print("No volumes up for removal.")
+        return
+
     volume_retention_date = TODAY - timedelta(days=days_to_retain_volume)
 
     for volume in VOLUME_RESOURCE:
